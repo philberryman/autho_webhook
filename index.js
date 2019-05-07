7e4fff92086503018acb03dc012a5bb1cf2b3c0d
Phil B <phil@berryman.org.uk>
2019-05-07 16:00:07 +0100

    updated url to https

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var requestClient = require('request');
+var requestClient = require("request");
 
-app.route('/').get((request, response) => {
-
-
-  var token = request.get('Authorization');
+app.route("/").get((request, response) => {
+  var token = request.get("Authorization");
 
   if (!token) {
-    response.json({'x-hasura-role': 'anonymous'});
+    response.json({ "x-hasura-role": "anonymous" });
     return;
   } else {
     // Fetch information about this user from
     // auth0 to validate this token
     // NOTE: Replace the URL with your own auth0 app url
     var options = {
-      url: `berryman.eu.auth0.com/userinfo`,
+      url: `https://berryman.eu.auth0.com/userinfo`,
       headers: {
         Authorization: token,
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     };
 
     requestClient(options, (err, res, body) => {
@@ -27,19 +25,21 @@ app.route('/').get((request, response) => {
         var userInfo = JSON.parse(body);
         console.log(userInfo); //debug
         var hasuraVariables = {
-          'X-Hasura-User-Id': userInfo.sub,
-          'X-Hasura-Role': 'user'
+          "X-Hasura-User-Id": userInfo.sub,
+          "X-Hasura-Role": "user",
         };
         console.log(hasuraVariables); // For debug
         response.json(hasuraVariables);
       } else {
         // Error response from auth0
         console.log(err, res, body);
-        response.json({'x-hasura-role': 'anonymous'});
+        response.json({ "x-hasura-role": "anonymous" });
         return;
       }
     });
   }
 });
 
-app.listen(3000,()=>{console.log('running')});
+app.listen(3000, () => {
+  console.log("running");
+});
